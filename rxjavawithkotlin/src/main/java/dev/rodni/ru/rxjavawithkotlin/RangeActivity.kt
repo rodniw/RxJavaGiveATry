de6ac2a841fa2fe0e89230e0beca51ee38fc0db4
rodniw <47295698+rodniw@users.noreply.github.com>
2019-08-12 22:02:35 +0300

    range with kotlin.

---
+++ rxjavawithkotlin/src/main/java/dev/rodni/ru/rxjavawithkotlin/RangeActivity.kt
@@ -0,0 +1,26 @@
+package dev.rodni.ru.rxjavawithkotlin
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observable
+
+class RangeActivity : AppCompatActivity() {
+
+    @SuppressLint("CheckResult")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //val ints = mutableListOf(1, 3, 5, 15)
+
+        getObservableRange().subscribe{
+                range -> println("received: $range")
+        }
+    }
+
+    private fun getObservableRange() : Observable<Int> {
+        //start point is 10 and it will count to 30 because range is 20
+        return Observable.range(10, 20)
+    }
+}
