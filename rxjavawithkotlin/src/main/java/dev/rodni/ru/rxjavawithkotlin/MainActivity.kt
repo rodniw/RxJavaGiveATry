35d7091b504c14f972b84d98e81960eccb9220f9
rodniw <47295698+rodniw@users.noreply.github.com>
2019-08-12 21:21:04 +0300

    the very first example of rxjava with kotlin.

--- rxjavawithkotlin/src/main/java/dev/rodni/ru/rxjavawithkotlin/MainActivity.kt
+++ rxjavawithkotlin/src/main/java/dev/rodni/ru/rxjavawithkotlin/MainActivity.kt
@@ -2,11 +2,35 @@ package dev.rodni.ru.rxjavawithkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataStream = Observable.just(10,20,30,40)
+
+        val dataObserver = object : Observer<Int> {
+            override fun onComplete() {
+                println("onComplete")
+            }
+            override fun onSubscribe(d: Disposable) {
+            }
+            override fun onNext(t: Int) {
+                println("onNext: $t")
+            }
+            override fun onError(e: Throwable) {
+                println("onError: ${e.message}")
+            }
+        }
+
+        dataStream
+                .subscribeOn(Schedulers.io())
+                .subscribe(dataObserver)
     }
 }
